3df92f441178182c4e69e445c53401005e0f6e3c
Anastasiia Tikhonova <anastasiia.tikhonova@metropolia.fi>
2020-10-25 20:51:19 +0200

    Added import Google Font styles.

--- src/components/vue-speed-meter.vue
+++ src/components/vue-speed-meter.vue
@@ -384,6 +384,16 @@ export default {
 </script>
 
 <style scoped>
+/* Import Google Font styles. */
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Kameron&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Copse&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Copse&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@100;300&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap");
+
 #main-container {
   height: var(--circle-size);
   width: var(--circle-size);
